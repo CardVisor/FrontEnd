d49316e253571ad6b7d73bd8f900aba3d9f4e0b6
Hwasoo-Jeon <royallhs@naver.com>
2024-02-11 05:28:45 +0900

    hsjeon benefitCluster 1차 chart

--- src/views/admin/benefit/components/BenefitTreemap.js
+++ src/views/admin/benefit/components/BenefitTreemap.js
@@ -1,45 +1,34 @@
-import React, { useRef, useLayoutEffect } from "react";
+import React, { useRef, useEffect } from "react";
 
 import * as am5 from "@amcharts/amcharts5";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 import * as am5hierarchy from "@amcharts/amcharts5/hierarchy";
-import { Container } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import Card from "components/card/Card.js";
 
-function BenefitTreemap({ data }) {
+function BenefitTreemap(props) {
   const chartDiv = useRef(null);
-  let root;
-  useLayoutEffect(() => {
+  const { data, val } = props;
+  useEffect(() => {
     if (chartDiv.current) {
-      root = am5.Root.new(chartDiv.current);
-
+      const root = am5.Root.new(chartDiv.current);
+      root.setThemes([am5themes_Animated.new(root)]);
       const myTheme = am5.Theme.new(root);
-
       myTheme
         .rule("RoundedRectangle", ["hierarchy", "node", "shape", "depth1"])
         .setAll({
-          strokeWidth: 2,
+          strokeWidth: 10, //선 굵기
         });
-
       myTheme
         .rule("RoundedRectangle", ["hierarchy", "node", "shape", "depth2"])
         .setAll({
-          fillOpacity: 0,
           strokeWidth: 1,
-          strokeOpacity: 0.2,
+          strokeOpacity: 1,
         });
 
-      myTheme.rule("Label", ["node", "depth1"]).setAll({
-        forceHidden: true,
-      });
+      root.setThemes([myTheme]);
 
-      myTheme.rule("Label", ["node", "depth2"]).setAll({
-        fontSize: 10,
-      });
-
-      root.setThemes([am5themes_Animated.new(root), myTheme]);
-
-      // Create wrapper container
-      let container = root.container.children.push(
+      const container = root.container.children.push(
         am5.Container.new(root, {
           width: am5.percent(100),
           height: am5.percent(100),
@@ -47,47 +36,61 @@ function BenefitTreemap({ data }) {
         })
       );
 
-      // 나머지 차트 설정 코드들...
-
-      let series = container.children.push(
+      const series = container.children.push(
         am5hierarchy.Treemap.new(root, {
+          data: data,
           sort: "descending",
-          singleBranchOnly: false,
+          singleBranchOnly: true,
           downDepth: 1,
           upDepth: 0,
           initialDepth: 1,
           valueField: "value",
           categoryField: "name",
           childDataField: "children",
+          layoutAlgorithm: "binary",
           nodePaddingOuter: 0,
           nodePaddingInner: 0,
+          fillField: "color",
         })
       );
 
-      series.get("colors").set("step", 1);
-      series.data.setAll(processData(data));
-      series.set("selectedDataItem", series.dataItems[0]);
+      /*
+      배경색 지정
+      *** 사용하시려면 하단의 series.data.setAll 부분에 color : "#FFFFFF" 형식으로 색상을 추가해야합니다.
+      */
+      series.rectangles.template.adapters.add("fill", function (fill, target) {
+        var customColor = target.dataItem.dataContext.color;
+        return customColor || fill;
+      });
 
-      // series.bullets.push(function (root, series, dataItem) {
-      //   let depth = dataItem.get("depth");
-
-      //   if (depth === 1) {
-      //     let picture = am5.Picture.new(root, {
-      //       src:
-      //         "https://www.amcharts.com/wp-content/uploads/assets/logos/" +
-      //         dataItem.dataContext.name.toLowerCase() +
-      //         ".png",
-      //       centerX: am5.p50,
-      //       centerY: am5.p50,
-      //       width: am5.percent(30),
-      //       isMeasured: true,
-      //     });
-
-      //     picture.states.lookup("default").setAll({ opacity: 0.15 });
-
-      //     return am5.Bullet.new(root, { sprite: picture });
-      //   }
-      // });
+      /*
+      툴팁(마우스 오버시 나오는 텍스트)
+     
+      var tooltip = am5.Tooltip.new(root, {
+        autoTextColor: false, //자동 색상 지정 false
+        getFillFromSprite: false,
+        labelText: "{name.children}", //툴팁 내용
+      });
+      //툴팁 배경
+      tooltip.get("background").setAll({
+        fill: am5.color(0xffffff),
+        fillOpacity: 0.8,
+      });
+      //툴팁 글씨 생상
+      tooltip.label.setAll({
+        //fill: "#4682B4",
+        fill: "#000000",
+      });
+      series.set("tooltip", tooltip);
+      */
+      /*
+      라벨(필드에 나타나는 텍스트)
+      []태그를 활용해 bold, fontSize 설정 가능
+      [/] 뒤 부터는 기본 폰트로 초기화
+      */
+      series.labels.template.setAll({
+        text: "[bold;fontSize:16px;]{name}",
+      });
 
       container.children.moveValue(
         am5hierarchy.BreadcrumbBar.new(root, {
@@ -95,61 +98,25 @@ function BenefitTreemap({ data }) {
         }),
         0
       );
-
-      // 나머지 차트 설정 코드들...
+      series.data.setAll(data);
+      //컴포넌트가 언마운트되거나 data가 변경될 때 실행
+      return () => {
+        // root가 존재하면 차트를 제거
+        root && root.dispose();
+      };
     }
-
-    return () => {
-      if (root) {
-        root.dispose();
-      }
-    };
   }, [data]);
 
-  return <div ref={chartDiv} style={{ width: "100%", height: "500px" }}></div>;
-}
-
-function processData(data) {
-  let treeData = [];
-
-  am5.object.eachOrdered(
-    data,
-    (brand) => {
-      let brandData = { name: brand, children: [] };
-      let brandTotal = 0;
-      for (var model in data[brand]) {
-        brandTotal += data[brand][model];
-      }
-
-      for (var model in data[brand]) {
-        // do not add very small
-        if (data[brand][model] > 100) {
-          brandData.children.push({ name: model, value: data[brand][model] });
-        }
-      }
-
-      // only bigger brands
-      if (brandTotal > 20) {
-        treeData.push(brandData);
-      }
-    },
-    (a, b) => {
-      let aval = 0;
-      let bval = 0;
-      am5.object.each(data[a], (key, val) => (aval += val));
-      am5.object.each(data[b], (key, val) => (bval += val));
-      if (aval > bval) return -1;
-      if (aval < bval) return 1;
-      return 0;
-    }
+  return (
+    <Card p="20px">
+      <Flex direction={{ base: "column" }} justify="center">
+        <Box mb={{ base: "1px", "2xl": "1px" }} position="relative">
+          <Text fontSize="xl">{val}</Text>
+          <div ref={chartDiv} style={{ width: "100%", height: "400px" }}></div>
+        </Box>
+      </Flex>
+    </Card>
   );
-
-  return [
-    {
-      name: "ALL TYPE",
-      children: treeData,
-    },
-  ];
 }
 
 export default BenefitTreemap;
--- src/views/admin/benefit/index.jsx
+++ src/views/admin/benefit/index.jsx
@@ -1,40 +1,110 @@
-import { Box, Grid, SimpleGrid } from '@chakra-ui/react';
-import React from 'react';
-import BenefitTreemap from './components/BenefitTreemap';
-import * as am5 from '@amcharts/amcharts5';
-  
-export default function Settings() {
-    let data = {
-        "Acura": {
-          "ILX": 11757,
-          "MDX": 54886,
-        },
-        "Alfa Romeo": {
-          "4C": 407,
-          "Giulia": 8903,
-          
-        }
-    };
+import { Box, Grid, SimpleGrid } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import BenefitTreemap from "./components/BenefitTreemap";
+import axios from "axios";
 
-    
-    return (
-        <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
-            <Grid
-                mb="30px"
-                mr="60px"
-                ml="60px"
-                templateColumns={{
-                    base: '1fr',
-                    lg: '1.2fr 3.5fr',
-                }}
-                templateRows={{
-                    base: 'repeat(1, 1fr)',
-                    lg: '1fr',
-                }}
-                gap={{ base: '20px', xl: '20px' }}
-            >
-                <BenefitTreemap data={data}></BenefitTreemap>
-            </Grid>
-        </Box>
-    );
+export default function Settings() {
+  const [benefitList, setBenefitList] = useState([]);
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "/benefitCluster/mcc",
+    })
+      .then((res) => {
+        console.log(res.data);
+        setBenefitList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  var data = [
+    {
+      name: "Beer styles",
+      children: [
+        {
+          name: "Pale Ales",
+          children: [
+            { name: "American Amber Ale", value: 1 },
+            { name: "American Pale Ale", value: 1 },
+            { name: "Blonde Ale", value: 1 },
+            { name: "English-Style Bitter", value: 1 },
+            { name: "English-Style", value: 1 },
+          ],
+        },
+        {
+          name: "Dark Lagers",
+          children: [
+            { name: "American Amber Lager", value: 1 },
+            { name: "German-Style Dunkel", value: 1 },
+            { name: "German-Style Marzen / Oktoberfest", value: 1 },
+            { name: "German-Style Schwarzbier", value: 1 },
+            { name: "Vienna-Style Lager", value: 1 },
+          ],
+        },
+        {
+          name: "Brown Ales",
+          children: [
+            { name: "American Brown Ale", value: 1 },
+            { name: "English-Style Brown Ale", value: 1 },
+            { name: "English-Style Mild", value: 1 },
+          ],
+        },
+        {
+          name: "India Pale Ales",
+          children: [
+            { name: "American IPA", value: 1 },
+            { name: "English-Style IPA", value: 1 },
+            { name: "Imperial India Pale Ale", value: 1 },
+            { name: "New England IPA", value: 1 },
+          ],
+        },
+        {
+          name: "Wheat Beers",
+          children: [
+            { name: "American-Style Wheat Wine Ale", value: 1 },
+            { name: "American Wheat", value: 1 },
+            { name: "Belgian-Style Witbier", value: 1 },
+            { name: "Berliner-Style Weisse", value: 1 },
+            { name: "German-Style Dunkelweizen", value: 1 },
+            { name: "German-Style Hefeweizen", value: 1 },
+          ],
+        },
+        {
+          name: "Strong Ales",
+          children: [
+            { name: "American Barley Wine", value: 1 },
+            { name: "American Imperial Red Ale", value: 1 },
+            { name: "British-S", value: 1 },
+            { name: "English-Style Old Ale", value: 1 },
+          ],
+        },
+      ],
+    },
+  ];
+
+  return (
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <Grid
+        mb="30px"
+        mr="60px"
+        ml="60px"
+        templateColumns={{
+          base: "1fr",
+          lg: "4.7fr",
+        }}
+        templateRows={{
+          base: "repeat(1, 1fr)",
+          lg: "1fr",
+        }}
+        gap={{ base: "20px", xl: "20px" }}
+      >
+        <BenefitTreemap
+          data={benefitList}
+          val={"Benefit 군집 요약"}
+        ></BenefitTreemap>
+      </Grid>
+    </Box>
+  );
 }
