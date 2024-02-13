adab09888c77877337cc4feaeb7589a5dfdde936
DESKTOP-3O6KIK7\User <eunkyung211@naver.com>
2024-02-13 11:29:41 +0900

    - international 중간 커밋

---
+++ src/views/admin/international/components/TopHighestOrderPayment.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import IconBox from "components/icons/IconBox";
+import { Box, Icon, Text, useColorModeValue } from "@chakra-ui/react";
+import InterMiniStatistics from "views/admin/international/components/InterMiniStatistics";
+import { FaChartPie } from "react-icons/fa6";
+import axios from "axios";
+import "assets/css/International.css";
+
+function TopHighestOrderPayment(props) {
+    const brandColor = useColorModeValue("brand.500", "white");
+    const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+    const [paymentRank, setPaymentRank] = useState(null);
+
+    //const data = ['data1', 'data2', 'data3', 'data4', 'data5', 'data6', 'data7', 'data8', 'data9', 'data10'];
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isVisible, setIsVisible] = useState(false);
+    
+    const numberWithCommas = (x)=> {
+        return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    useEffect(() => {
+        axios({
+            method: "get",
+            url: "/international/highestOrderPayment",
+        })
+            .then((res) => {
+                console.log(res.data);
+                setPaymentRank(res.data);
+                setIsVisible(true);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+    
+    useEffect(() => {
+        if (paymentRank !== null) {
+            const timer = setInterval(() => {
+                setIsVisible(false);
+    
+                // currentIndex 상태 변경 후 약간의 딜레이를 준다.
+                setTimeout(() => {
+                    setCurrentIndex((currentIndex) => (currentIndex + 1) % paymentRank.length);
+                    setIsVisible(true);
+                }, 500);
+            }, 3000);
+    
+            return () => clearInterval(timer);
+        }
+    }, [paymentRank]);
+    
+    return (
+        <>
+          <InterMiniStatistics
+            startContent={
+              <IconBox
+                w="56px"
+                h="56px"
+                bg={boxBg}
+                icon={
+                  <Icon
+                    w="32px"
+                    h="32px"
+                    as={FaChartPie}
+                    color={brandColor}
+                  />
+                }
+              />
+            }
+            name={paymentRank && `올해 결제 건별 나라 순위`}
+            value={
+              <Box className={isVisible ? 'fade-in' : 'fade-out'}>
+                {paymentRank && (
+                  <>
+                    <span className="toplist">No.{currentIndex + 1}</span>
+                    {paymentRank[currentIndex].CURRENCY_NATION.split(" ")[0]}{" "}
+                    {numberWithCommas(paymentRank[currentIndex].PAYMENT_CNT)}건
+                  </>
+                )}
+              </Box>
+            }
+          />
+        </>
+      );
+}
+
+export default TopHighestOrderPayment;
---
+++ src/views/admin/international/components/TopTotalPayment.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import IconBox from "components/icons/IconBox";
+import { Icon, useColorModeValue } from "@chakra-ui/react";
+import InterMiniStatistics from "views/admin/international/components/InterMiniStatistics";
+import { GiMoneyStack } from "react-icons/gi";
+import axios from "axios";
+
+
+function TopTotalPayment(props) {
+    const brandColor = useColorModeValue("brand.500", "white");
+    const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+    const [totalPayment, setTotalPayment]  = useState();
+
+    const numberWithCommas = (x)=> {
+        return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    useEffect(()=>{
+        axios({
+            method: "get",
+            url:"/international/totalPayment",
+        }).then((res)=>{
+            console.log("??");
+            console.log(res.data);
+            setTotalPayment(res.data)
+        }).catch((err)=>{
+            console.log(err);
+        });
+    },[]);
+
+    return (
+        <>
+            <InterMiniStatistics
+                startContent={
+                    <IconBox
+                        w="56px"
+                        h="56px"
+                        bg={boxBg}
+                        icon={
+                            <Icon
+                                w="32px"
+                                h="32px"
+                                as={GiMoneyStack}
+                                color={brandColor}
+                            />
+                        }
+                    />
+                }
+                name={totalPayment && '연간 해외 총 결제 금액'}
+                value={totalPayment && `\u{20A9} ${numberWithCommas(totalPayment)} 원`}
+            />
+        </>
+    );
+}
+
+export default TopTotalPayment;
