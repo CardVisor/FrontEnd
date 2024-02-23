eea317d1cc9b409baf2769e7f7d83b8c226cdf88
DESKTOP-3O6KIK7\User <eunkyung211@naver.com>
2024-02-23 23:39:32 +0900

    - benefit 중간 커밋

--- src/views/admin/benefit/components/Column.js
+++ src/views/admin/benefit/components/Column.js
@@ -1,44 +1,62 @@
 import { Droppable } from "react-beautiful-dnd";
 import Item from "./Item";
-import { styled } from "./stitches.config";
+import styled from "styled-components";
+import { Box } from "@chakra-ui/react";
 
-const StyledColumn = styled("div", {
-  padding: "24px 16px",
-  display: "flex",
-  flexDirection: "column",
-  marginTop: 8,
+const CateUnitWrap = styled.div`
+    padding: 10px 7px;
 
-  h2: {
-    margin: 0,
-    padding: "0 16px",
-  },
-});
+    &.basketItem {
+      background-coloe: pink;
+    }
 
-const StyledList = styled("div", {
-  backgroundColor: "#ddd",
-  borderRadius: 8,
-  padding: 10,
-  display: "flex",
-  flexDirection: "column",
-  // flexGrow: 1,
-  marginTop: 3,
-  height: 200,
-  width: 250,
-});
+    & .cateUnitTit {
+        font-size: 14px;
+        font-weight: bold;
+        padding-bottom: 5px;
+        padding-left: 5px;
+    }
+    & .cateUnitBody {
+        min-height: 200px;
+        background-color: #f5f5f5;
+        border-radius: 8px;
+        padding: 10px;
+    }
+    & .cateUnitFoot {
+        font-size: 14px;
+        background: #fffad0;
+        & span {
+            color: #3f51b5;
+            font-size: 18px;
+            font-weight: bold;
+        } 
+    }
+`;
+
+const formatNumber = (number) => {
+  if (number > 0) {
+      return new Intl.NumberFormat('ko-KR').format(number);
+  } else if (number === 0) {
+      return '-';
+  }
+};
 
 function Column({ col }) {
   return (
     <Droppable droppableId={col.id}>
       {(provided) => (
-        <StyledColumn>
-          <h2>{col.id}</h2>
-          <StyledList {...provided.droppableProps} ref={provided.innerRef}>
-            {col.list.slice(0, 3).map((item, index) => (
-              <Item key={item.benefit_id} item={item} index={index} />
-            ))}
-            {provided.placeholder}
-          </StyledList>
-        </StyledColumn>
+        <CateUnitWrap className={col.id === "신규 조합" ? "basketItem" : ""}>
+              <h2 className="cateUnitTit">{col.id}</h2>
+              <Box className="cateUnitBody" {...provided.droppableProps} ref={provided.innerRef}
+                  
+              >
+                  {col.list.slice(0, 3).map((item, index) => (
+                    <Item key={item.benefit_id} item={item} index={index} />
+                  ))}
+                  {provided.placeholder}
+                  {col.id === "신규 조합" && <span className="cateUnitFoot">카드 조합에 따른 혜택<br />가치 합:&nbsp;&nbsp;<span>{formatNumber(226111)}</span>&nbsp;원</span>}
+              </Box>
+          </CateUnitWrap>
       )}
     </Droppable>
   );
